246354cd164d4d7a5be2e00d87224d4b64b397c5
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-07-13 19:18:42 +0900

    I have added the attributes for the class Account

---
+++ cpp_banking.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cctype>
+
+using namespace std;
+
+class Account {
+    int accNum; //This variable will hold the account number
+    int deposit; //This variable will hold the amount of the deposit
+    char type; //This variable will hold the type of account
+    char accName[60]; //This variable hold the name of the account holder
+
+    
+
+}
+
+
+
+int main(){
+    
+
+    return 0;
+}
